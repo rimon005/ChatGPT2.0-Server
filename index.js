d51016e9e26f6acb4b82ea9fe25cff48f140bc05
Asif Iqbal Rimon <ariyan8185@gmail.com>
2023-01-30 20:54:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import env from 'dotenv';
+import {Configuration , OpenAIApi} from 'openai';
+
+
+const app = express();
+
+env.config();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+const configuration = new Configuration({
+    organization: "org-25vLvJIbZVXNl2OKOBQLR6EA",
+    apiKey: process.env.API_KEY
+});
+
+console.log(process.env.API_KEY);
+
+const openai = new OpenAIApi(configuration)
+
+app.listen('3080' , () => {
+    console.log('listening on port 3080');
+})
+
+
+app.get('/' , (req ,res) => {
+    res.send('Hello world !')
+});
+
+
+
+// post route for making requests
+
+
+app.post('/' , async(req , res ) => {
+    const {massage} = req.body
+
+    try{
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt:`${massage}`,
+            max_tokens: 100,
+            temperature : .5
+        })
+        res.json({massage : response.data.choices[0].text})
+
+    }catch(e){
+        console.log(e);
+        res.send(e).status(400)
+    }
+})
